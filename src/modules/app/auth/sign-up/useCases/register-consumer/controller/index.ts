ce7b1d80751ec1668c7f8857f3f309df9a2adbbf
iago fagundes <iago.fagundes@outlook.com>
2022-04-11 13:15:01 -0300

    feat: clear cache on save

--- src/modules/app/auth/sign-up/useCases/register-consumer/controller/index.ts
+++ src/modules/app/auth/sign-up/useCases/register-consumer/controller/index.ts
@@ -1,17 +1,31 @@
 import { Body, Controller, Post } from '@nestjs/common';
+import { CacheService } from '@src/modules/common/services';
 import { User } from '@src/modules/database/entities';
 import { RegisterConsumerDTO } from '../dtos';
 import { RegisterConsumerService } from '../service';
 
 @Controller()
 export class RegisterConsumerController {
-  constructor(private readonly service: RegisterConsumerService) {}
+  constructor(
+    private readonly service: RegisterConsumerService,
+    private readonly cacheService: CacheService
+  ) {}
+
+  private async clearCache(user: User): Promise<void> {
+    const document = `@document:${user.document}`;
+    const email = `@email:${user.email}`;
+    const phone = `@phone:${user.phone}`;
+
+    this.cacheService.del(document, email, phone).catch(err => console.error(err));
+  }
 
   @Post()
   public async exec(@Body() body: RegisterConsumerDTO): Promise<User> {
     const registerOrError = await this.service.exec(body);
 
     if (registerOrError.isLeft()) throw registerOrError.value;
+
+    this.clearCache(registerOrError.value);
 
     return registerOrError.value;
   }
--- src/modules/app/auth/sign-up/useCases/register-producer/controller/index.ts
+++ src/modules/app/auth/sign-up/useCases/register-producer/controller/index.ts
@@ -1,11 +1,23 @@
 import { Body, Controller, Post } from '@nestjs/common';
+import { CacheService } from '@src/modules/common/services';
 import { User } from '@src/modules/database/entities';
 import { RegisterProducerDTO } from '../dtos';
 import { RegisterProducerService } from '../service';
 
 @Controller()
 export class RegisterProducerController {
-  constructor(private readonly service: RegisterProducerService) {}
+  constructor(
+    private readonly service: RegisterProducerService,
+    private readonly cacheService: CacheService
+  ) {}
+
+  private async clearCache(user: User): Promise<void> {
+    const document = `@document:${user.document}`;
+    const email = `@email:${user.email}`;
+    const phone = `@phone:${user.phone}`;
+
+    this.cacheService.del(document, email, phone).catch(err => console.error(err));
+  }
 
   @Post()
   public async exec(@Body() body: RegisterProducerDTO): Promise<User> {
