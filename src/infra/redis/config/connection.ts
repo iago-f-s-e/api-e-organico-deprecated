b31e255f10dae9c18fe4dae8148e742d1898bc61
iago-f-s-e <iago.fagundes@outlook.com>
2022-07-01 17:43:56 -0300

    fix: redis tls

--- src/infra/redis/config/connection.ts
+++ src/infra/redis/config/connection.ts
@@ -1,6 +1,9 @@
 import Redis from 'ioredis';
 import * as Settings from '@src/server/settings';
 
+const tls = Settings.IS_PRODUCTION ? { rejectUnauthorized: false } : undefined;
+
 export const redisConnection = new Redis(Settings.REDIS_URI, {
-  connectTimeout: Settings.REDIS_TIMEOUT
+  connectTimeout: Settings.REDIS_TIMEOUT,
+  tls
 });
