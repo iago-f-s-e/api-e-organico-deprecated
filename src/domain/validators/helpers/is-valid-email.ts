471f981f2113a2137f1912a2aa732a43783d96c4
iago-f-s-e <iago.fagundes@outlook.com>
2022-07-04 17:20:27 -0300

    fix: validate email

--- src/domain/validators/helpers/is-valid-email.ts
+++ src/domain/validators/helpers/is-valid-email.ts
@@ -1,7 +1,7 @@
 export const isValidEmail = (value: string, maxSize: number): boolean => {
   if (value.length > maxSize) return false;
 
-  const tester = /^([a-z0-9]+[.|-|_]?)*[a-z0-9]\@[a-z0-9]+(\.[a-z]+)+$/;
+  const tester = /^([a-zA-Z0-9]+[.|\-|_]?)*[a-zA-Z0-9]\@[a-zA-Z0-9]+(\.[a-zA-Z]+)+$/;
 
   return tester.test(value);
 };
